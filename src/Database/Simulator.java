b336db399aa486435768a46780b31f9b1bbbefd2
Your Name <jennifer.luk21@ycdsbk12.ca>
2021-01-19 12:52:49 -0500

    created welcome menu

--- src/Database/Simulator.java
+++ src/Database/Simulator.java
@@ -5,36 +5,10 @@ import java.util.ArrayList;
 
 public class Simulator {
     public static void main(String[] args) throws IOException{
-        ArrayList<DataReader> data = readData("src/Database/Dataset.csv");
-
-        for (DataReader b : data) {
-            System.out.println(b); 
-        }
-
-        // BufferedReader file = new BufferedReader(new FileReader("src/Database/BothSexes.csv"));
-        // String date;
-        // String province;
-        // String labour;
-        // String sex;
-        // String population; 
-
-
-        // while(file.readLine() != null){
-        //     String data[] = file.readLine().split(",");
-        //     date = data[0];
-        //     province = data[1];
-        //     labour = data[2];
-        //     sex = data[3];
-        //     population = data[4];
-            
-        //     // System.out.println(date + "|" + province + "|" + labour + "|" + sex + "|" +population);
-        // }
-        // file.close();
-    }
-    private static ArrayList<DataReader> readData(String fileName) throws IOException{
+        
         ArrayList<DataReader> data = new ArrayList<DataReader>();
-        BufferedReader file = new BufferedReader(new FileReader(fileName));
-      
+        BufferedReader file = new BufferedReader(new FileReader("src/Database/DataSet.csv"));
+        BufferedReader key = new BufferedReader(new InputStreamReader(System.in));      
 
         while(file.readLine() != null){
             String datalist[] = file.readLine().split(",");
@@ -44,9 +18,18 @@ public class Simulator {
 
         }
         file.close();
-
-        return data;
         
+        System.out.println("\nWelcome to the Stats Canada Labour Force Characteristics System");
+        System.out.println("Please select one of the options below: ");
+        System.out.println("\n(1) View Data Sorted by Province");
+        System.out.println("(2) View Data Sorted by Labour Type");
+        System.out.println("(3) View Data with Filters");
+        System.out.println("(4) View an Individual Record by Month & Year");
+        System.out.println("(5) View Summary Report");
+        System.out.println("(6) View Charts");
+
+        System.out.print("Input Choice: ");
+        String strChoice = key.readLine();
     }
 
     private static DataReader createData(String[] datalist) { 
@@ -61,7 +44,7 @@ public class Simulator {
         labour = datalist[2];
         sex = datalist[3];
         population = datalist[4];
-        
+ 
         // create and return book of this metadata return new 
         return new DataReader(date, province, labour, sex, population); 
     }
